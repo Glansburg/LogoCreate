d8f9a6a52945fe141cd4f13ecb1266d5b552133c
Joel Getzke <joelcode2023@gmail.com>
2023-07-15 15:25:48 -0500

    code broken?

--- Library/index.js
+++ Library/index.js
@@ -24,7 +24,6 @@ inquirer.prompt(logo)
     .then(data => {
         console.log(data)
         const svg = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-
         <circle cx="150" cy="100" r="80" fill="green" />
       
         <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">${data.text}</text>
