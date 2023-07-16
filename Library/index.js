979b5eba51f99f4692239dfe17b688ea1f151175
Joel Getzke <joelcode2023@gmail.com>
2023-07-15 20:44:14 -0500

    fs.write file error fix

--- Library/index.js
+++ Library/index.js
@@ -1,5 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+//import colors from 'color-name';
 
 // take this information and make it into a string
 const logo = [
@@ -10,12 +11,21 @@ const logo = [
         validate: function (characters) {
             if (characters.length <= 3) {
                 return true;
-            } 
-            return "please enter 3 characters to begin"
-        }
+            }
+            return "please enter 3 characters to begin";
+        },
+
+       // type: 'input',
+      //  name: 'text-color',
+       // message: 'type in your favorite color for logo.',
+        
 
 
     }
+    
+    
+       
+    
 ]
 
 // TODO: Create a function to initialize app
@@ -23,6 +33,7 @@ const logo = [
 inquirer.prompt(logo)
     .then(data => {
         console.log(data)
+        
         const svg = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
         <circle cx="150" cy="100" r="80" fill="green" />
       
@@ -31,28 +42,30 @@ inquirer.prompt(logo)
     
       
       </svg>`
-      console.log(svg);
+        console.log(svg);
+        
+        
+fs.writeFile('svgLogo.svg', svg(data), (err) =>
+err ? console.error(err) : console.log('Success'))
     })
 
 //use fs write file 
 
-fs.writeFile('svgLogo.svg', svg(data), (err) => 
-err ? console.error(err) : console.log ('Success'))
 // step 1 Function call to initialize app
 
-function init() { 
-    inquirer.prompt(logo)
-    .then(data => svg(data))
-}
-
 // Function call to initialize app
-init();
+//function init() {
+    //inquirer.prompt(logo)
+        //.then(data => svg(data))
+//}
+
+// Function call to initialize app invocked.
+//init();
 
 
-// Function call to initialize app
 
 
-// Function call to initialize app
+
 
 
 //use classes for shapes only they should have a function on them called render that returns a string that contains shape content <circle cx="150" cy="100" r="80" fill="green" />
